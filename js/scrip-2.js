22aed3eb0460b4e1c872579c84f7c760fcc50d0e
kristalgamarra <kristal.gamarra@gmail.com>
2026-03-13 12:57:03 -0500

    Add files via upload

---
+++ js/scrip-2.js
@@ -0,0 +1,5 @@
+let enviar = document.getElementById("Enviar");
+
+Enviar.onclick = function(){
+    alert("Envio realizado");
+}
---
+++ js/script-1.js
@@ -0,0 +1,15 @@
+let compra1 = document.getElementById("compra-1");
+let compra2 = document.getElementById("compra-2");
+let compra3 = document.getElementById("compra-3");
+
+compra1.onclick = function(){
+    alert("Compra realizada");
+}
+
+compra2.onclick = function(){
+    alert("Compra realizada");
+}
+
+compra3.onclick = function(){
+    alert("Compra realizada");
+}
