b765aa35b0f6f2f010b6ebc4a83b14b403620320
kristalgamarra <kristal.gamarra@gmail.com>
2026-03-13 17:09:02 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ let btn3 = document.getElementById("select-3");
 let imgchange = document.getElementById("imgchange");
 
 btn1.onclick = function(){
-    imgchange.src = "../imagenes/banner2.jpg"
+    imgchange.src = "imagenes/banner2.jpg"
 }
 
 btn2.onclick = function(){
-    imgchange.src = "../imagenes/banner1.jpg"
+    imgchange.src = "imagenes/banner1.jpg"
 }
 
 btn3.onclick = function(){
-    imgchange.src = "../imagenes/banner3.jpg"
+    imgchange.src = "imagenes/banner3.jpg"
 }
